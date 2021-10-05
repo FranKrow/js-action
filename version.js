77e34aeb581f674642b1cab46aabb1c4a50ae03a
Franco Daniel <dafc14@hotmail.com>
2021-10-05 13:02:39 -0500

    Release data

--- version.js
+++ version.js
@@ -5,11 +5,22 @@ const run = async () =>  {
     core.info("Setting the input and env variables");
 
     const root = process.env.GITHUB_WORKSPACE;
-    const file = core.getInput("files").replace(" ","").split(",");
+    const tag = (process.env.GITHUB_REF).replace('refs/tags/', '');
+    const files = core.getInput("files").replace(" ","").split(",");
+    const regex = new RegExp(core.getInput("version-regexp"));
+    const token = core.getInput('repo-token');
+
+    const octokit = github.getOctokit(token);
+    const { owner, repo } = github.context.repo;
+
+    const release = await octokit.repos.getReleaseByTag({ owner, repo, tag });
 
     console.log(`root ${root}`);
+    console.log(`files ${files}`);
+    console.log(`regex ${regex}`);
+    console.log(`token ${token}`);
+    console.log(`release ${release}`);
 
-    console.log(`file ${file}`);
 }
 
 run()
